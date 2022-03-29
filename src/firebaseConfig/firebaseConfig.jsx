385af74e46465f9d015d792b02167bb82b816f86
BraianRosales <56799523+BraianRosales@users.noreply.github.com>
2022-03-29 20:02:02 -0300

    Configuracion proyecto.

--- src/firebaseConfig/firebaseConfig.jsx
+++ src/firebaseConfig/firebaseConfig.jsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
+  apiKey: "AIzaSyCcGoANia0sqlV7OczKvX1ISImLzQ6qriQ",
+  authDomain: "cotodigitalclon.firebaseapp.com",
+  projectId: "cotodigitalclon",
+  storageBucket: "cotodigitalclon.appspot.com",
+  messagingSenderId: "76577900064",
+  appId: "1:76577900064:web:22f96c0226b63acfe4de45",
 };
 
 const app = initializeApp(firebaseConfig);
